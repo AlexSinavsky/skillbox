906a1def564d9e5fe700e35c640ee8149a5332ae
alexS <sinavsky@mail.ru>
2022-08-20 15:42:41 +0300

    task 10. Done

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,11 +1,9 @@
 base = [1, 4, -3, 0, 10]
-baseNew = []
-long = len(base)
-longNew = len(baseNew)
-minNum = base[0]
-for numb in base:
-    if numb < minNum:
-        minNum = numb
-baseNew.append(minNum)
+print('Неотсортированный список', base)
+long = int(len(base))
+for i in range(len(base)):
+    for numb in range(i + 1, len(base)):
+        if base[i] > base[numb]:
+            base[i], base[numb] = base[numb], base[i]
 
-print(baseNew)
+print('Отсортированный список', base)
