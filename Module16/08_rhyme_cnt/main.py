87ac4e2e988edb6556bc0e23b007a53108d51cee
alexS <sinavsky@mail.ru>
2022-08-23 09:32:34 +0300

    task 8, 10 Need help

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+numb = 5#int(input('Кол-во человек: '))
+numbOut = 7#int(input('Какое число в считалке? '))
+print('Значит, выбывает каждый', numbOut, 'человек')
+numbList = list(range(1, numb + 1))
+nextRoundOut = numbOut - len(numbList) - 1
+start = 1
+while len(numbList) > 1:
+    for i in range(start, len(numbList)):
+        print('\nТекущий круг людей:', numbList)
+        print('Начало счёта с номера', start)
+        print('Выбывает человек под номером:', numbList[nextRoundOut])
+        numbList.pop(nextRoundOut)
+        nextRoundOut = numbOut - len(numbList)
+        start = nextRoundOut
+        if nextRoundOut > len(numbList):
+            nextRoundOut = abs(numbOut - len(numbList))
+            start = nextRoundOut
+
+print(numbList)
+
--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+numb = int(input('Количество цифр: '))
+
+numbBase = []
+for _ in range(numb):
+    numbers = int(input('Число: '))
+    numbBase.append(numbers)
+print('Последовательность', numbBase)
+
+count = 0
+for i_numb in range(len(numbBase)-1, 0, -1):
+    print(numbBase[i_numb], numbBase[i_numb - 1])
+    if numbBase[i_numb] == numbBase[i_numb - 1]:
+        count += 1
+#    print(i_numb, end='')
+print('Нужно приписать чисел:', count)
