9150f9a946524742ce5f494f4258e9ac329d3e41
alexS <sinavsky@mail.ru>
2022-08-24 13:10:28 +0300

    task 10 done

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,20 +1,14 @@
-numb = 5#int(input('Кол-во человек: '))
-numbOut = 7#int(input('Какое число в считалке? '))
-print('Значит, выбывает каждый', numbOut, 'человек')
+numb = int(input('Кол-во человек: '))
+numbOut = int(input('Какое число в считалке? '))
+print('Значит, выбывает каждый', numbOut, 'человек.')
 numbList = list(range(1, numb + 1))
-nextRoundOut = numbOut - len(numbList) - 1
-start = 1
-while len(numbList) > 1:
-    for i in range(start, len(numbList)):
-        print('\nТекущий круг людей:', numbList)
-        print('Начало счёта с номера', start)
-        print('Выбывает человек под номером:', numbList[nextRoundOut])
-        numbList.pop(nextRoundOut)
-        nextRoundOut = numbOut - len(numbList)
-        start = nextRoundOut
-        if nextRoundOut > len(numbList):
-            nextRoundOut = abs(numbOut - len(numbList))
-            start = nextRoundOut
+out = 0
 
-print(numbList)
-
+for step in range(numb - 1):
+    print('Текущий круг людей', numbList)
+    start_count = out % len(numbList)
+    out = (start_count + numbOut - 1) % len(numbList)
+    print('Начало счёта с номера', numbList[start_count])
+    print('Выбывает человек под номером', numbList[out])
+    numbList.remove(numbList[out])
+print('Остался человек под номером', numbList)
--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,15 +1,29 @@
-numb = int(input('Количество цифр: '))
+def is_palindrome(base):
+    reversBase = []
+    for num in range(len(base) -1, -1, -1):
+        reversBase.append(base[num])
+    if base == reversBase:
+        return True
+    else:
+        return False
 
+numb = int(input('Количество цифр: '))
 numbBase = []
 for _ in range(numb):
     numbers = int(input('Число: '))
     numbBase.append(numbers)
-print('Последовательность', numbBase)
 
-count = 0
-for i_numb in range(len(numbBase)-1, 0, -1):
-    print(numbBase[i_numb], numbBase[i_numb - 1])
-    if numbBase[i_numb] == numbBase[i_numb - 1]:
-        count += 1
-#    print(i_numb, end='')
-print('Нужно приписать чисел:', count)
+new_nums = []
+answer = []
+for i_nums in range(0, len(numbBase)):
+    for j_elem in range(i_nums, len(numbBase)):
+        new_nums.append(numbBase[j_elem])
+    if is_palindrome(new_nums):
+        for i_answer in range(0, i_nums):
+            answer.append(numbBase[i_answer])
+        answer.reverse()
+        break
+    new_nums = []
+print('Исходный список:', numbBase)
+print('Нужно чисел для палиндрома:', len(answer))
+print('Список этих чисел:', answer)
