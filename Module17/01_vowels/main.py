58c0ad8eed6ad62b11d5817e84c9bad371c34b70
Алексей Синявский <sinavsky@mail.ru>
2022-08-29 12:40:10 +0000

    Update main.py

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,7 +1,7 @@
 vowelBase = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
 text = input('Введите текст: ')
-vowelInText = []
-for letters in text:
-  if letters in vowelBase:
-    vowelInText.append(letters)
+vowelInText = [letters for letters in text if letters in vowelBase]
+
 print('Список гласных букв', vowelInText,'\nДлина списка:', len(vowelInText))
+
+            
